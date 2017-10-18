08c0257a7c8bf6e8c71d450ef880416cf0434b47
FrancescoNegri <francesco.negri@outlook.com>
2017-10-18 09:51:59 +0200

    working on restaurantDetailsPage

--- src/components/RestaurantDetailsPage/index.js
+++ src/components/RestaurantDetailsPage/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { ActivityIndicator, Button, ListView, Text, View } from 'react-native';
 
-export default class RestaurantsListPage extends Component {
+export default class RestaurantDetailsPage extends Component {
 
     constructor(props) {
         super(props);
@@ -12,7 +12,7 @@ export default class RestaurantsListPage extends Component {
 
     componentDidMount() {
         const { navigate } = this.props.navigation;
-        return fetch('https://just-eat-backend-francesconegri.c9users.io:8080/restaurants')
+        return fetch('https://just-eat-backend-francesconegri.c9users.io:8080/restaurants/' + this.props.navigation.state.params["id"])
             .then((response) => {
                 if (response.status === 200) return response.json()
                 else alert("API unreachable!");
@@ -22,7 +22,7 @@ export default class RestaurantsListPage extends Component {
                     let ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
                     this.setState({
                         isLoading: false,
-                        dataSource: ds.cloneWithRows(responseJson["RESTAURANTS"]),
+                        dataSource: ds.cloneWithRows(responseJson["MENU"]),
                     }, function () {
                         // do something with new state
                     });
@@ -37,7 +37,6 @@ export default class RestaurantsListPage extends Component {
     }
 
     render() {
-        const { navigate } = this.props.navigation;
         if (this.state.isLoading) {
             return (
                 <View style={{ flex: 1, paddingTop: 20 }}>
@@ -50,9 +49,7 @@ export default class RestaurantsListPage extends Component {
             <View style={{ flex: 1, paddingTop: 20 }}>
                 <ListView
                     dataSource={this.state.dataSource}
-                    renderRow={(rowData) => <Button title={rowData["NAME"]} onPress={() => {
-                        navigate('RestaurantDetails', { id: rowData["ID"] });
-                    }} />}
+                    renderRow={(rowData) => <Button title={rowData["NAME"]} onPress={() => alert(rowData["DESCRIPTION"])} />}
                 />
             </View>
         );
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { StackNavigator } from 'react-navigation';
 import HomePage from './components/HomePage';
 import RestaurantsListPage from './components/RestaurantsListPage';
+import RestaurantDetailsPage from './components/RestaurantDetailsPage';
 
 const JustEatApp = StackNavigator({
   Home: { screen: HomePage },
   RestaurantsList: { screen: RestaurantsListPage },
+  RestaurantDetails: { screen: RestaurantDetailsPage}
 });
 
 export default class App extends React.Component {
